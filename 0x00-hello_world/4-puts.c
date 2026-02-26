3db8a399343b2ec420840b00f09f67dbac94e3fa
Ae7is <aejis777@gnail.com>
2026-02-26 02:03:28 +0100

    Corrected my script according to betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 /**
  * main - Entry Point
  *
- * Return Always 0
+ * Return: Always 0
  */
- int main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
